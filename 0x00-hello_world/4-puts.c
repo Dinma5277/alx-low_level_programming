7366c2f153bb40afc021a6be0361b8e74de21851
Dinma5277 <ndukwechidinmastephanie@gmail.com>
2023-04-20 22:02:53 -0700

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
  * Description:  main - Prints out a string to stdout.
- * Return: 0 if success.
+ * Return: 0 if Success.
  */
 int main(void)
 {
-puts(*\*Programming is like building a multilingual puzzle*);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
