b6dd6449fb37b5b84543a03cb682e4ef10e74192
Dinma5277 <ndukwechidinmastephanie@gmail.com>
2023-04-25 13:06:25 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * 2-print_alphabet_x10 - Print lowercase alphabet 10 times
+ * print_alphabet_x10 - Print lowercase alphabet 10 times
  */
 
 void print_alphabet_x10(void)
 {
-	char num,letter;
+	char num, letter;
 
 	for (num = 0; num <= 9; num++)
 	{
@@ -17,3 +17,4 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 	}
 }
+
